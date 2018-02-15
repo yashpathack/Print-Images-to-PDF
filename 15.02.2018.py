4db604a02e01481e96736c16135148aedd91c9b9
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-02-15 13:20:13 +0530

    Update 15.02.2018.py

--- 15.02.2018.py
+++ 15.02.2018.py
@@ -1,10 +1,28 @@
+#This is the command line to be implemented
+#Scripts and 'image' will be imported from the PIL
 from PIL import Image
+
+#Import OS to command the Operating System
 import os
 
-filename = "marvin.gif"
-im = Image.open(filename)
-if im.mode == "RGBA":
-   im = im.convert("RGB")
-new_filename = "sql1.pdf"
-if not os.path.exists(new_filename):
-    im.save(new_filename, "PDF", resolution = 1000.0)
+#Note: In this case the image and the 15.02.2018.py are present in the same directory. Therefore I need not to copy the full path for the cause. 
+#The image format can vary within JPG/JPEG, PNG and GIF. SVG is under process, for that you can import svglib.svglib
+pic = "thenameoftheimage.gif"
+
+#The Navigator opens the picture.
+photo = Image.open(pic)
+
+#Changing the mode from RGBA
+if photo.mode == "RGBA":
+   
+   #To RGB
+   photo = photo.convert("RGB")
+   
+#Assignage of new file generated   
+picv2 = "thenameoftheimage.pdf"
+
+if not os.path.exists(picv2):
+   #Set the resolution and initialize the Portable Document Format
+    photo.save(picv2, "PDF", resolution = 100.0)
+
+#Run the Module. A new file with the specified name will be generated. Open it with any of the pdf reader. Adios!
